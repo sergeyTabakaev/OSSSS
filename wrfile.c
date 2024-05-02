51131e08c107007eae6d0ae952297962a9d05795
sergeyTabakaev <tabakaev.ca2003@gmail.com>
2024-05-02 12:11:00 +0000

    Sozdali fail zapisyvayushii stroku v fail wrfile.txt

--- wrfile.c
+++ wrfile.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+
 int main() {
 	int fd;
 	size_t size;
@@ -11,59 +12,17 @@ int main() {
 		printf("Oshibka pri otkritii faila na zapis'\n");
 		exit(-1);
 	}
+
 	size = write(fd, string, 14);
 	if(size != 14){
 		printf("Ne udalos zapisat 14 bait v fail\n");
 		exit(-1);
 	}
 	else printf("Stroka zapisana v fail\n");
-	if(close(fd) < 0) {
-		printf("Ne poluchilos zakrit fail\n");
-		exit(-1);
-	}
-
-
-
-	char resstring[14];
-	if((fd = open("wrfile.txt", O_RDONLY)) < 0) {
-		printf("Oshibka pri otkritii faila na chtenie'\n");
-		exit(-1);
-	}
-
-	size = read(fd, resstring, 14);
-	if(size != 14){
-		printf("Ne udalos prochitat 14 bait iz faila\n");
-		exit(-1);
-	}
-	else printf("Prochitannaya stroka %s\n", resstring);
 
 	if(close(fd) < 0) {
 		printf("Ne poluchilos zakrit fail\n");
 		exit(-1);
 	}
-
-	return 0;
-}
-
-
-
-	char resstring[14];
-	if((fd = open("wrfile.txt", O_RDONLY)) < 0) {
-		printf("Oshibka pri otkritii faila na chtenie'\n");
-		exit(-1);
-	}
-
-	size = read(fd, resstring, 14);
-	if(size != 14){
-		printf("Ne udalos prochitat 14 bait iz faila\n");
-		exit(-1);
-	}
-	else printf("Prochitannaya stroka %s\n", resstring);
-
-	if(close(fd) < 0) {
-		printf("Ne poluchilos zakrit fail\n");
-		exit(-1);
-	}
-
 	return 0;
 }
