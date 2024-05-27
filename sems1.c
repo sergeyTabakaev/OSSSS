e99c8185bce7b3fdddc23098aa1715742abb4380
sergeyTabakaev <tabakaev.ca2003@gmail.com>
2024-05-27 12:08:00 +0000

    Dobavili programmu dlya raboti s semaforami

--- sems1.c
+++ sems1.c
@@ -1,3 +1,4 @@
+
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
@@ -7,26 +8,6 @@
 int main() {
 	int semid;
 	char pathname[] = "fifo.c";
+	char pathname[] = "key.ipc";
 	key_t key;
 	struct sembuf mybuf;
-
-	if((key = ftok(pathname, 0)) < 0){
-		printf("Ne udalos sozdat kluch\n");
-		exit(-1);
-	}
-
-	if((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0) {
-		printf("Ne udalos poluchit deskriptor semaforov\n");
-		exit(-1);
-	}
-
-	mybuf.sem_op = -1;
-	mybuf.sem_flg = 0;
-	mybuf.sem_num = 0;
-	if(semop(semid, &mybuf, 1) < 0){
-		printf("Ne udalos vipolnit operaciyu nad semaforom\n");
-		exit(-1);
-	}
-	printf("Uslovie razblokirovki processa viyavleno\n");
-	return 0;
-}
